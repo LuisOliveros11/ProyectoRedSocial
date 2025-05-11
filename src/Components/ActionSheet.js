c61b03fd7b52cb4ca65b939f903feb47adca7f2d
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2025-05-10 17:45:39 -0700

    Settings: Actualizar imagen de perfil

--- src/Components/ActionSheet.js
+++ src/Components/ActionSheet.js
@@ -1,41 +1,149 @@
 // ActionSheet.js
-import React, { forwardRef, useImperativeHandle, useRef } from 'react';
+import React, { forwardRef, useImperativeHandle, useRef, useState, useContext } from 'react';
 import { TouchableOpacity, Text, StyleSheet, View } from 'react-native';
 import RBSheet from 'react-native-raw-bottom-sheet';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import * as ImagePicker from 'expo-image-picker';
-
+import { AuthContext } from './AuthContext';
 
 const ActionSheet = forwardRef((props, ref) => {
   const sheetRef = useRef();
-  
+  const { authToken, userData, updateUserData } = useContext(AuthContext);
+
+
+
+  const [title, setDescription] = useState(
+    props.title || 'Hacer una publicación'
+  );
+
 
   useImperativeHandle(ref, () => ({
-    open: () => sheetRef.current.open(),
+    open: (newDescription) => {
+      if (newDescription) setDescription(newDescription);
+      sheetRef.current.open();
+    },
     close: () => sheetRef.current.close()
   }));
-  const pickImage = async () => {
-    try {
-      let result = await ImagePicker.launchCameraAsync({
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
+
+  const uploadImage = async (imageUri) => {
+    const formData = new FormData();
+    const fileName = imageUri.split('/').pop();
+    const match = /\.(\w+)$/.exec(fileName ?? '');
+    const fileType = match ? `image/${match[1]}` : `image`;
+    formData.append('photo', {
+      uri: imageUri,
+      name: fileName,
+      type: fileType
+    });
+
+    const maxRetries = 3;
+    let retryCount = 0;
+
+    const timeout = (ms, promise) => {
+      return new Promise((resolve, reject) => {
+        const timer = setTimeout(() => {
+          reject(new Error("La solicitud tardó demasiado. Intenta con una imagen más liviana."));
+        }, ms);
+
+        promise
+          .then((res) => {
+            clearTimeout(timer);
+            resolve(res);
+          })
+          .catch((err) => {
+            clearTimeout(timer);
+            reject(err);
+          });
       });
+    };
+
+    const attemptUpload = async () => {
+      try {
+        const response = await timeout(60000, fetch(`http://192.168.1.250:3000/actualizarUsuario/${userData.id}`, {
+          method: 'PUT',
+          body: formData,
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'multipart/form-data',
+          },
+        }));
+
+        if (!response.ok) {
+          throw new Error(`Error HTTP: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        if (data.user && data.user.photo) {
+          await updateUserData({
+            ...userData,
+            photo: data.user.photo,
+          });
+          alert("Los datos se han actualizado exitosamente");
+          return true;
+        } else {
+          throw new Error("No se recibió la información de la foto actualizada");
+        }
+      } catch (error) {
+        if (retryCount < maxRetries - 1) {
+          retryCount++;
+          await new Promise(resolve => setTimeout(resolve, 2000));
+          return attemptUpload();
+        }
+        throw error;
+      }
+    };
+
+    try {
+      await attemptUpload();
     } catch (error) {
-      console.error('Error al tomar la foto:', error);
-      alert('Error al procesar la imagen');
+      let errorMessage = 'Error al subir la imagen. ';
+      
+      if (error.message.includes('Network request failed')) {
+        errorMessage += 'Por favor, verifica tu conexión a internet e intenta nuevamente.';
+      } else if (error.message.includes('tardó demasiado')) {
+        errorMessage += 'La imagen es demasiado pesada. Intenta con una imagen más liviana.';
+      } else {
+        errorMessage += 'Por favor, intente nuevamente.';
+      }
+      
+      setTimeout(() => {
+        alert(errorMessage);
+        sheetRef.current?.close();
+      }, 0);
     }
   };
   const pickImageGallery = async () => {
     try {
-      let result = await ImagePicker.launchImageLibraryAsync({
+      const result = await ImagePicker.launchImageLibraryAsync({
         allowsEditing: true,
         aspect: [4, 3],
         quality: 1,
       });
+
+      if (!result.canceled) {
+        const imageUri = result.assets[0].uri;
+        await uploadImage(imageUri);
+      }
     } catch (error) {
-      console.error('Error al tomar la foto:', error);
-      alert('Error al procesar la imagen');
+      sheetRef.current?.close();
+    }
+  };
+
+  const pickImage = async () => {
+    try {
+      const result = await ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        const imageUri = result.assets[0].uri;
+        await uploadImage(imageUri);
+      }
+    } catch (error) {
+      sheetRef.current?.close();
     }
   };
 
@@ -44,19 +152,21 @@ const ActionSheet = forwardRef((props, ref) => {
       ref={sheetRef}
       height={360}
       openDuration={250}
-      draggable={true} 
+      draggable={true}
       closeOnPressMask={true}
       customStyles={{ container: styles.sheet }}
     >
       <View style={styles.sheetContent}>
         <FeatherIcon name="image" color="#2b64e3" size={48} style={{ alignSelf: 'center' }} />
-        <Text style={styles.title}>Hacer una publicación</Text>
+        <Text style={styles.title}>
+          {title}
+        </Text>
         <Text style={styles.message}>
           Seleccione una foto desde su galería o tome una foto
         </Text>
 
         <TouchableOpacity onPress={() => {
-            pickImageGallery();
+          pickImageGallery();
         }}>
           <View style={styles.btn}>
             <Text style={styles.btnText}>Abrir galería</Text>
@@ -64,9 +174,9 @@ const ActionSheet = forwardRef((props, ref) => {
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => {
-            pickImage();
+          pickImage();
         }}>
-          <View style={[styles.btn, { marginTop: 12, backgroundColor: '#FFF',     borderWidth: 1, }]}>
+          <View style={[styles.btn, { marginTop: 12, backgroundColor: '#FFF', borderWidth: 1, }]}>
             <Text style={[styles.btnText, { color: '#2b64e3' }]}>Tomar foto</Text>
           </View>
         </TouchableOpacity>
@@ -114,5 +224,5 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     color: '#fff',
   },
- 
+
 });
--- src/tabs/SettingsScreen.js
+++ src/tabs/SettingsScreen.js
@@ -1,211 +1,218 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { StyleSheet, SafeAreaView, View, Image, Text, TouchableOpacity, ScrollView, Button } from 'react-native';
 import FeatherIcon from 'react-native-vector-icons/Feather'
 
 import { AuthContext } from '../Components/AuthContext';
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+import ActionSheet from '../Components/ActionSheet';
 
 const SettingsScreen = () => {
   const navigation = useNavigation();
-  const {authToken, userData} = useContext(AuthContext);
+  const { authToken, userData } = useContext(AuthContext);
+  const sheetRef = useRef();
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: "#fff"}}>
-      <ScrollView contentContainerStyle={styles.container}>
-        <View style={styles.profile}>
-          <TouchableOpacity onPress={() =>{
-            //Reedirigir a pantalla
-          }}>
-          <View style={styles.profileAvatarWrapper}>
-            <Image
-              alt="foto de perfil" 
-              source= {{ uri: userData.photo }}
-              style={styles.profileAvatar}  
-            />
-          </View>
-          <View style={styles.profileAction}>
-            <FeatherIcon name="edit-3" size={15} color="#fff" />
-          </View>
-
-          </TouchableOpacity>
-          <Text style={styles.profileName}>{userData.name}</Text>
-          <Text style={styles.profileAddress}>{userData.email}</Text>
-        </View>
-        
-        {/* Secciones que tendrá el apartado de ajustes/settings */}
-        <View style={styles.section}>
-          <Text style={styles.sectionHeader}>Cuenta</Text>
-          <TouchableOpacity onPress={() =>{
-            //Reedirigir a pantalla
+    <>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+        <ScrollView contentContainerStyle={styles.container}>
+          <View style={styles.profile}>
+            <TouchableOpacity onPress={() => {
+              sheetRef.current.open('Sube una foto de perfil');
             }}>
-            <View style={styles.row}>
-              <View style={[styles.rowIcon, { backgroundColor: "#134ded" }]}>
-                <FeatherIcon name="bell" color="#fff" size={18}></FeatherIcon>
+              <View style={styles.profileAvatarWrapper}>
+                <Image
+                  alt="foto de perfil"
+                  source={{ uri: userData.photo }}
+                  style={styles.profileAvatar}
+                />
               </View>
-              <Text style={[styles.rowLabel, { flex: 1 }]}>Notificaciones</Text>
+              <View style={styles.profileAction}>
+                <FeatherIcon name="edit-3" size={15} color="#fff" />
+              </View>
+
+            </TouchableOpacity>
+            <Text style={styles.profileName}>{userData.name}</Text>
+            <Text style={styles.profileAddress}>{userData.email}</Text>
+          </View>
+
+          {/* Secciones que tendrá el apartado de ajustes/settings */}
+          <View style={styles.section}>
+            <Text style={styles.sectionHeader}>Cuenta</Text>
+            <TouchableOpacity onPress={() => {
+              //Reedirigir a pantalla
+            }}>
+              <View style={styles.row}>
+                <View style={[styles.rowIcon, { backgroundColor: "#134ded" }]}>
+                  <FeatherIcon name="bell" color="#fff" size={18}></FeatherIcon>
+                </View>
+                <Text style={[styles.rowLabel, { flex: 1 }]}>Notificaciones</Text>
 
 
-              <FeatherIcon
+                <FeatherIcon
                   name="chevron-right"
                   color="#0c0c0c"
                   size={22}
-              />        
-            </View>
-
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() =>{
-            //Reedirigir a pantalla
-            }}>
-            <View style={styles.row}>
-              <View style={[styles.rowIcon, { backgroundColor: "#134ded" }]}>
-                <FeatherIcon name="bookmark" color="#fff" size={18}></FeatherIcon>
+                />
               </View>
-              <Text style={[styles.rowLabel, { flex: 1 }]}>Publicaciones guardadas</Text>
+
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+              //Reedirigir a pantalla
+            }}>
+              <View style={styles.row}>
+                <View style={[styles.rowIcon, { backgroundColor: "#134ded" }]}>
+                  <FeatherIcon name="bookmark" color="#fff" size={18}></FeatherIcon>
+                </View>
+                <Text style={[styles.rowLabel, { flex: 1 }]}>Publicaciones guardadas</Text>
 
 
-              <FeatherIcon
+                <FeatherIcon
                   name="chevron-right"
                   color="#0c0c0c"
                   size={22}
-              />        
-            </View>
+                />
+              </View>
 
-          </TouchableOpacity>
-        </View>
+            </TouchableOpacity>
+          </View>
 
-        <View style={styles.section}>
-          <Text style={styles.sectionHeader}>Datos de la cuenta</Text>
-          <TouchableOpacity onPress={() =>{
+          <View style={styles.section}>
+            <Text style={styles.sectionHeader}>Datos de la cuenta</Text>
+            <TouchableOpacity onPress={() => {
               navigation.navigate('Change password');
             }}>
-            <View style={styles.row}>
-              <View style={[styles.rowIcon, { backgroundColor: "#134ded" }]}>
-                <FeatherIcon name="lock" color="#fff" size={18}></FeatherIcon>
-              </View>
-              <Text style={[styles.rowLabel, { flex: 1 }]}>Cambiar Contraseña</Text>
+              <View style={styles.row}>
+                <View style={[styles.rowIcon, { backgroundColor: "#134ded" }]}>
+                  <FeatherIcon name="lock" color="#fff" size={18}></FeatherIcon>
+                </View>
+                <Text style={[styles.rowLabel, { flex: 1 }]}>Cambiar Contraseña</Text>
 
 
-              <FeatherIcon
+                <FeatherIcon
                   name="chevron-right"
                   color="#0c0c0c"
                   size={22}
-              />        
-            </View>
+                />
+              </View>
 
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() =>{
-            navigation.navigate('Edit profile');
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+              navigation.navigate('Edit profile');
             }}>
-            <View style={styles.row}>
-              <View style={[styles.rowIcon, { backgroundColor: "#134ded" }]}>
-                <FeatherIcon name="edit-3" color="#fff" size={18}></FeatherIcon>
-              </View>
-              <Text style={[styles.rowLabel, { flex: 1 }]}>Editar perfil</Text>
+              <View style={styles.row}>
+                <View style={[styles.rowIcon, { backgroundColor: "#134ded" }]}>
+                  <FeatherIcon name="edit-3" color="#fff" size={18}></FeatherIcon>
+                </View>
+                <Text style={[styles.rowLabel, { flex: 1 }]}>Editar perfil</Text>
 
 
-              <FeatherIcon
+                <FeatherIcon
                   name="chevron-right"
                   color="#0c0c0c"
                   size={22}
-              />        
-            </View>
+                />
+              </View>
 
-          </TouchableOpacity>
-        </View>
+            </TouchableOpacity>
+          </View>
 
-        <View style={styles.section}>
-          <Text style={styles.sectionHeader}>Sesión</Text>
-          <TouchableOpacity onPress={() =>{
-            //Reedirigir a pantalla
+          <View style={styles.section}>
+            <Text style={styles.sectionHeader}>Sesión</Text>
+            <TouchableOpacity onPress={() => {
+              //Reedirigir a pantalla
             }}>
-            <View style={styles.row}>
-              <View style={[styles.rowIcon, { backgroundColor: "#134ded" }]}>
-                <FeatherIcon name="log-out" color="#fff" size={18}></FeatherIcon>
-              </View>
-              <Text style={[styles.rowLabel, { flex: 1 }]}>Cerrar sesión</Text>
+              <View style={styles.row}>
+                <View style={[styles.rowIcon, { backgroundColor: "#134ded" }]}>
+                  <FeatherIcon name="log-out" color="#fff" size={18}></FeatherIcon>
+                </View>
+                <Text style={[styles.rowLabel, { flex: 1 }]}>Cerrar sesión</Text>
 
 
-              <FeatherIcon
+                <FeatherIcon
                   name="chevron-right"
                   color="#0c0c0c"
                   size={22}
-              />        
-            </View>
+                />
+              </View>
 
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+
+      <ActionSheet ref={sheetRef} />
+    </>
+
   );
 };
 
 export default SettingsScreen;
 
 const styles = StyleSheet.create({
-    container: {
-      paddingVertical: 24,
-    },
-    profile: {
-      padding: 24,
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    profileName: {
-      fontSize: 21,
-    },
-    profileAddress: {
-      marginTop: 5,
-      fontSize: 16,
-      color: '#989898',
-      textAlign: 'center'
-    },
-    profileAvatar: {
-      width: 90,
-      height: 90,
-      borderRadius: 9999,
-    },
-    profileAvatarWrapper: {
-      position: 'relative'
-    },
-    profileAction: {
-      width: 28,
-      height: 28,
-      borderRadius: 9999,
-      backgroundColor: '#007bff',
-      right: -60,
-      bottom: 20,
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    section: {
-      paddingHorizontal: 24,
-    },
-    sectionHeader: {
-      paddingVertical: 12,
-      fontSize: 12,
-      fontWeight: '600',
-      color: '#9e9e9e',
-      textTransform: 'uppercase',
-      letterSpacing: 1.1,
-    },
-    row: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-      height: '50',
-      backgroundColor: '#f2f2f2',
-      borderRadius: 8,
-      marginBottom: 12,
-      paddingHorizontal: 12
-    },
-    rowIcon: {
-      width: 32,
-      height: 32,
-      borderRadius: 999,
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginRight: 12
-    },
-    arrowIcon: {
-      alignItems: 'flex-end'
-    }
-  });
+  container: {
+    paddingVertical: 24,
+  },
+  profile: {
+    padding: 24,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  profileName: {
+    fontSize: 21,
+  },
+  profileAddress: {
+    marginTop: 5,
+    fontSize: 16,
+    color: '#989898',
+    textAlign: 'center'
+  },
+  profileAvatar: {
+    width: 90,
+    height: 90,
+    borderRadius: 9999,
+  },
+  profileAvatarWrapper: {
+    position: 'relative'
+  },
+  profileAction: {
+    width: 28,
+    height: 28,
+    borderRadius: 9999,
+    backgroundColor: '#007bff',
+    right: -60,
+    bottom: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  section: {
+    paddingHorizontal: 24,
+  },
+  sectionHeader: {
+    paddingVertical: 12,
+    fontSize: 12,
+    fontWeight: '600',
+    color: '#9e9e9e',
+    textTransform: 'uppercase',
+    letterSpacing: 1.1,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: '50',
+    backgroundColor: '#f2f2f2',
+    borderRadius: 8,
+    marginBottom: 12,
+    paddingHorizontal: 12
+  },
+  rowIcon: {
+    width: 32,
+    height: 32,
+    borderRadius: 999,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 12
+  },
+  arrowIcon: {
+    alignItems: 'flex-end'
+  }
+});
